5353ca7da963e8fc7ac44a3291b81da1487dc783
Alex Campos <alexcamposve@gmail.com>
2024-10-05 08:36:32 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionicaxion',
+  webDir: 'www'
+};
+
+export default config;
